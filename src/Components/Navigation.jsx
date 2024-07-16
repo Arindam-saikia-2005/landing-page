08db08260d40ed990529d5a2b1986c63f38ab396
SparkWeb-Official <sparkweb2022@gmail.com>
2024-07-16 11:06:49 +0530

    css changes

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,19 +1,18 @@
-
-export default function Navigation()  {
-    return (
-        <div>
-        <nav className="container">
-        <div className="logo">
-          <img src="./images/brand_logo.jpg" alt="logo"/>
-        </div>
-        <ul>
-          <li href="#">Menu</li>
-          <li href="#">Location</li>
-          <li href="#">About</li>
-          <li href="#">Contact</li>
-        </ul>
-        <button>login</button>
-      </nav>
-        </div>
-    )
+export default function Navigation() {
+	return (
+		<div>
+			<nav className='container'>
+				<div className='logo'>
+					<img src='./images/brand_logo.jpg' alt='logo' />
+				</div>
+				<ul>
+					<li href='#'>Menu</li>
+					<li href='#'>Location</li>
+					<li href='#'>About</li>
+					<li href='#'>Contact</li>
+				</ul>
+				<button>Login</button>
+			</nav>
+		</div>
+	);
 }
